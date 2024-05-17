245d440caef90999c7f7ecc77dc3814b67bd8a0a
Jack Webdev <jackbuda31@gmail.com>
2024-05-17 09:52:48 +0200

    auth bug fixed

--- app/api/(auth)/register/route.ts
+++ app/api/(auth)/register/route.ts
@@ -1,11 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 import db from "@/database/index";
 import { hashPassword } from "@/lib/auth";
+import { isValidEmailDomain } from "@/lib/validateEmail";
+import { signJwt } from "@/lib/jwt";
+import { cookies } from "next/headers";
 
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
     const data = await req.json();
     const { email, password } = await data;
+
+    if (!isValidEmailDomain(email, "ndt.co.za")) {
+      return NextResponse.json({
+        message: "Invalid NDT email. Please try again",
+      });
+    }
 
     const hashedPassword = await hashPassword(password, 10);
 
@@ -18,7 +27,14 @@ export async function POST(req: NextRequest, res: NextResponse) {
       },
     });
 
-    return NextResponse.json({ message: user }, { status: 201 });
+    const token = signJwt({ id: user?.id, role: user?.Role }, "JWT_KEY!", 10);
+
+    cookies().set("jwtToken", token, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60,
+    });
+
+    return NextResponse.json(user, { status: 201 });
   } catch (error) {
     return NextResponse.json({ error: error }, { status: 500 });
   }
--- app/api/departments/route.ts
+++ app/api/departments/route.ts
@@ -14,7 +14,7 @@ export async function GET() {
 				},
 				users: {
 					select: {
-						id:true
+						
 					}
 				}
 			},
