bb533a972010f63ab3cb3c585244ea1326d2d9d0
Jack Webdev <jackbuda31@gmail.com>
2024-05-21 13:19:40 +0200

    timesheetTable

--- app/users/employee/timesheet/page.tsx
+++ app/users/employee/timesheet/page.tsx
@@ -7,6 +7,42 @@ import { format } from "date-fns";
 import axios from "axios";
 import { getSession } from "@/actions";
 
+import * as React from "react"
+import {
+  DotsHorizontalIcon,
+} from "@radix-ui/react-icons"
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+
 type Timesheet = {
   Friday: string;
   Monday: string;
@@ -22,6 +58,8 @@ type Timesheet = {
   Approval_Status: string;
 };
 
+
+
 const Timesheet = () => {
   const [selectedStartDate, setSelectedStartDate] = useState<Date | null>(
     new Date()
@@ -29,7 +67,80 @@ const Timesheet = () => {
   const [selectedEndDate, setSelectedEndDate] = useState<Date | null>(
     new Date()
   );
-  const [filteredTimesheets, setFilteredTimesheets] = useState<Timesheet[]>([]);
+  const [data, setFilteredTimesheets] = useState<Timesheet[]>([]);
+  const [sorting, setSorting] = React.useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  )
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({})
+  const [rowSelection, setRowSelection] = React.useState({})
+  const columns: ColumnDef<Timesheet>[] = [
+    {
+      accessorKey: "Approval_Status",
+      header: "Approval Status",
+      cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("Approval_Status")}</div>
+      ),
+    },
+    {
+      accessorKey: "Project_Name",
+      header:"Project Name",
+      cell: ({ row }) => <div className="lowercase">{row.getValue("Project_Name")}</div>,
+    },
+    {
+      accessorKey: "Task_performed",
+      header: "Task Performed",
+      cell: ({ row }) => <div className="lowercase">{row.getValue("Task_performed")}</div>,
+    },
+    {
+      accessorKey: "actions",
+      header: () => <div className="text-start">Actions</div>,
+      cell: ({ row }) => {
+  
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <DotsHorizontalIcon className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem
+              >
+                Copy payment ID
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>View customer</DropdownMenuItem>
+              <DropdownMenuItem>View payment details</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )
+      },
+    },
+  
+  ]
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  })
+
 
 
   useEffect(() => {
@@ -97,7 +208,7 @@ const Timesheet = () => {
         <h2 className="text-center text-3xl text-secondary">Your Timesheets</h2>
 
         <div className="timesheets-container mx-auto grid grid-cols-3 gap-x-20 gap-y-8">
-          {filteredTimesheets.map((timesheet) => (
+          {/* {filteredTimesheets.map((timesheet) => (
             <div
               className="card__container h-fit bg-white border-2 border-primary rounded-xl p-4"
               key={timesheet.id}
@@ -137,7 +248,93 @@ const Timesheet = () => {
                 </div>
               </>
             </div>
-          ))}
+          ))} */}
+              <div className="w-full">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter emails..."
+          value={(table.getColumn("Project_Name")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("Project_Name")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex-1 text-sm text-muted-foreground">Showing {" "}
+          {table.getFilteredSelectedRowModel().rows.length} to {" "}
+          {table.getFilteredRowModel().rows.length} out of 20 records.
+        </div>
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
         </div>
       </main>
     </div>
