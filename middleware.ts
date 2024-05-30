69b473ba9fcf01725e877aa67bdcf7b5d79706d1
Jack Webdev <jackbuda31@gmail.com>
2024-05-30 10:39:53 +0200

    middleware

--- middleware.ts
+++ middleware.ts
@@ -9,8 +9,27 @@ type Token = {
 	exp: string;
 };
 
+const allowedOrigins = ['https://nextjs-timesheet.vercel.app/']
+ 
+const corsOptions = {
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+}
+
 export function middleware(request: NextRequest) {
 	const token = request.cookies.get("jwtToken");
+	const origin = request.headers.get('origin') ?? ''
+	const isAllowedOrigin = allowedOrigins.includes(origin)
+   
+	const isPreflight = request.method === 'OPTIONS'
+   
+	if (isPreflight) {
+	  const preflightHeaders = {
+		...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
+		...corsOptions,
+	  }
+	  return NextResponse.json({}, { headers: preflightHeaders })
+	}
 
 	if (token?.value) {
 		try {
