03d2cc28746301c061ec6304979f09dede0848b5
Jack Webdev <jackbuda31@gmail.com>
2024-06-11 06:30:33 +0200

    uploadThing

--- components/addEmployee/Documents.tsx
+++ components/addEmployee/Documents.tsx
@@ -1,7 +1,109 @@
-import React from 'react'
+"use client";
+
+import { UploadDropzone } from "@/utils/uploadThing";
+import { Label } from "../ui/label";
+import { toast } from "react-toastify";
+
 
 export default function Documents() {
   return (
-    <div>Documents</div>
+    <div className="grid grid-cols-2 gap-4">
+      <div>
+        <Label htmlFor="name" className="mb-1">
+          Documents
+        </Label>
+        <UploadDropzone
+                  appearance={{
+                    label:"text-primary hover:text-secondary",
+                    button:"bg-primary text-white rounded-xl",
+                  }}
+          className=" border border-primary rounded-xl cursor-pointer custom-class"
+          endpoint="imageUploader"
+          onClientUploadComplete={(res) => {
+            // Do something with the response
+            console.log("Files: ", res);
+            toast.success("Upload Completed");
+          }}
+          onUploadError={(error: Error) => {
+            // Do something with the error.
+            toast.error(`ERROR! ${error.message}`);
+          }}
+          />
+      </div>
+
+      <div>
+        <Label htmlFor="name" className="mb-1">
+          Letter of Recommendation
+        </Label>
+        <UploadDropzone
+                  appearance={{
+                    label:"text-primary hover:text-secondary",
+                    button:"bg-primary text-white rounded-xl",
+
+                  }}
+ 
+          className=" border border-primary rounded-xl cursor-pointer custom-class"
+          endpoint="imageUploader"
+          onClientUploadComplete={(res) => {
+            // Do something with the response
+            console.log("Files: ", res);
+            toast.success("Upload Completed");
+          }}
+          onUploadError={(error: Error) => {
+            // Do something with the error.
+            toast.error(`ERROR! ${error.message}`);
+          }}
+          />
+      </div>
+
+      <div>
+        <Label htmlFor="name" className="mb-1">
+          Bank Statement
+        </Label>
+        <UploadDropzone
+                  appearance={{
+                    label:"text-primary hover:text-secondary",
+                    button:"bg-primary text-white rounded-xl",
+                  }}
+          className=" border border-primary rounded-xl cursor-pointer custom-class"
+          endpoint="imageUploader"
+          onClientUploadComplete={(res) => {
+            // Do something with the response
+            console.log("Files: ", res);
+            toast.success("Upload Completed");
+          }}
+          onUploadError={(error: Error) => {
+            // Do something with the error.
+            toast.error(`ERROR! ${error.message}`);
+          }}
+          />
+      </div>
+
+      <div>
+        <Label htmlFor="name" className="mb-1">
+          Resume
+        </Label>
+        <UploadDropzone
+          appearance={{
+            label:"text-primary hover:text-secondary",
+            button:"bg-primary text-white rounded-xl",
+          }}
+          className=" border border-primary rounded-xl cursor-pointer custom-class"
+          endpoint="imageUploader"
+          onClientUploadComplete={(res) => {
+            // Do something with the response
+            console.log("Files: ", res);
+            toast.success("Upload Completed");
+          }}
+          onUploadError={(error: Error) => {
+            // Do something with the error.
+            toast.error(`ERROR! ${error.message}`);
+          }}
+          />
+      </div>
+
+
+
+    </div>
   )
 }
