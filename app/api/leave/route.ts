3654f438a066a7199b067cf54605bda9874e3b1b
Jack Webdev <jackbuda31@gmail.com>
2024-07-07 14:14:01 +0200

    approval process and leave view table done

--- app/api/leave/route.ts
+++ app/api/leave/route.ts
@@ -6,7 +6,6 @@ export const revalidate = 0;
 export async function GET() {
   try {
     const res = await db.leaveRequest.findMany();
-
     return NextResponse.json(res, { status: 200 });
   } catch (error) {
     return NextResponse.json(error, { status: 500 });
